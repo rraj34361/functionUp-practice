1adec5f0b797ba1e90b785e76e4e0790e0ae10b4
rraj34361 <rraj34361@gmail.com>
2023-04-20 20:12:06 +0530

    assignment on accessing request as a variable

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,10 +3,20 @@ const { route } = require('express/lib/application');
 const router = express.Router();
 const commonFile = require('./common')
 const myUnderscore = require('underscore')
+const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+const films = [
+    {id: 1, name :'The shining'},
+    {id: 2, name :'Incendies'},
+    {id: 3, name :'Rang de basanti'},
+    {id: 4, name :'Finding Nemo'},
+
+]
+
 
 router.get('/test-me', function (req, res) {
     res.send('This should be working!')
 });
+
 
 router.get('/test-you', function (req, res) {
     console.log('The exported module is: ',commonFile)
@@ -20,6 +30,50 @@ router.get('/test-underscore', function(req, res){
     console.log('the result is',result)
     res.send('done')
 })
+//problem-1
+router.get('/movies', function(req, res){
+    res.send(movies)
+
+})
+
+//problem-2 and 3
+
+router.get('/movies/:indexNumber', function(req, res){
+    let index = Number(req.params.indexNumber)
+    if(index>=movies.length||index<0){
+        res.send('invalid indexNumber Please enter a valid indexNumber')
+    }
+    res.send(movies[index])
+    
+})
+
+
+
+//problem-4
+router.get('/films', function(req, res){
+    res.send(films)
+
+})
+
+//problem-5
+
+router.get('/films/:filmid', function(req, res){
+       let id = Number(req.params.filmid)
+       if(id>=films.length||id<=0){
+        res.send('No movie exists with this id')
+       }
+       else{
+       res.send(films[id-1])
+       }
+
+})
+
+
+
+
+
+
+
 
 router.get('/cohorts', function (request, response){
     // logic to get the cohorts from database
